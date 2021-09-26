39405ca1889ac9391666d7b5fb362920ed0afe87
Denis Rebrov <denisrebrof@gmail.com>
2021-09-27 01:49:54 +0300

    some common setting shit - need set up gitignore!!

---
+++ Assets/Scripts/GameLevels/Domain/GameLevelsUseCases.cs
@@ -0,0 +1,17 @@
+﻿namespace GameLevels.Domain
+{
+    public class LevelsUseCases
+    {
+        private ILevelsRepository _levelsRepository;
+        private IBalanceRepository _balanceRepository;
+
+        GameLevel GetLevel(int id)
+        {
+            return _levelsRepository.GetLevel(id);
+        }
+        
+        
+        
+        
+    }
+}
